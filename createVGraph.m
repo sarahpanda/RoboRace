ca1f42b9f4234b34f083e580861e1ed4e6ccaa89
Gaurav Ahuja <ga2371@columbia.edu>
2014-11-15 15:31:08 -0500

    Initial commit

--- createVGraph.m
+++ createVGraph.m
@@ -1,27 +1,31 @@
-function cleanedVertices = createVGraph(vertices)
+function cleanPolygons = createVGraph(polygons)
 
-deleted = [];
+    cleanPolygons = {};
 
-% discard vertices within polygons
-for k = 2:size(vertices,2)
-  thisPolygon = vertices{k};
-  
-  for i = 2:size(vertices,2)
-      if i == k
-          continue;
-      end
-      otherVertices = vertices{i};
-      
-      for j = size(otherVertices,1):-1:1
-          if inpoly(otherVertices(j,1), otherVertices(j,2), thisPolygon(:,1), thisPolygon(:,2))~=0
-             deleted = [deleted; otherVertices(j,:)];
-             otherVertices(j,:)=[];
-          end
-          
-      end
-      vertices{i} = otherVertices;
-  end
-  
-end
-cleanedVertices = vertices;
+    % discard polygons within polygons
+    for p =1:length(polygons)
+        thisPolygon = polygons{p};
+        newPoly = [];
+        
+        for v = 1:size(thisPolygon,1)
+            x = thisPolygon(v, 1);
+            y = thisPolygon(v, 2);
+            insidePoly = 0;
+            for op=1:size(polygons, 1)
+                if op == p
+                    continue;
+                end
+                if inpoly(x, y, polygons{op}(:, 1), polygons{op}(:, 2)) == 1
+                    insidePoly = 1;
+                    break;
+                end
+            end
+            if insidePoly == 0
+                newPoly  = [newPoly; x, y];
+            end
+        end
+        cleanPolygons{p} = newPoly;
+    end
+    length(cleanPolygons)
+    
 end
---
+++ drawPolygons.m
@@ -0,0 +1,19 @@
+function []=  drawPolygons(f, polygons, colour, marker)
+    for p = 1:length(polygons)
+        polygon = polygons{p};
+        
+        for k = 1:size(polygon,1)-1
+            p1 = polygon(k,:);
+            p2 = polygon(k+1,:);
+
+            line([p1(1) p2(1)],[p1(2) p2(2)], 'Color', colour, 'Marker', marker);
+            hold on;
+        end
+        if size(polygon,1) > 0
+            p1 = polygon(size(polygon,1),:);
+            p2 = polygon(1,:);
+            line([p1(1) p2(1)],[p1(2) p2(2)], 'Color', colour, 'Marker', marker);
+            hold on;
+        end
+    end
+end
